4dbc543aeec8af8c6196d579a5caf76341dc2e86
660710611 <pulkasem_p@silpakorn.edu>
2026-02-09 01:05:44 +0700

    first commit answer1,answer2

---
+++ first_project/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:first_project/page_answer/answer1.dart';
+import 'package:first_project/page_answer/answer2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Assignment',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+        useMaterial3: true,
+      ),
+      home: Answer2(),);
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Time App'),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      ),
+      backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+
+      body: 
+      Answer2(),
+
+
+    );
+  }
+}
